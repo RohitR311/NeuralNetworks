13738a8dfb419cd92613fa6385fa6722bcd91a4d
RohitR311 <rohit.rajan031101@gmail.com>
2021-10-03 22:35:22 +0530

    Code Sets

--- Chpt_18/.ipynb_checkpoints/Activation_Functions-checkpoint.py
+++ Chpt_18/.ipynb_checkpoints/Activation_Functions-checkpoint.py
@@ -15,6 +15,7 @@ class Activation_ReLU:
     
 class Activation_Softmax:
     def forward(self, inputs, training):
+        self.inputs = inputs
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         probabilties = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilties
